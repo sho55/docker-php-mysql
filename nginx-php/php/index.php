e67542a75c70a218c2b8b8d55ee77e4f9d0ad9a0
門馬　將平 <gra2546nd@gmail.com>
2019-09-08 13:53:09 +0900

    editing index file

--- nginx-php/php/index.php
+++ nginx-php/php/index.php
@@ -1,21 +1,24 @@
 <?php
 
 //接続する設定を作成
-$dsn = 'mysql:dbname=mysql;host=172.22.3.1;port=3316';
+$dsn = 'mysql:dbname=mysql;host=[YOUR GATEWAY IP];port=3316';
 $user = 'mysql';
 $password = 'mysql';
+
 
 try {
     $dbh = new PDO($dsn, $user, $password);
     
 
-    $sql = "SELECT COUNT( * ) FROM test_users";
+
+    $sql = "SELECT * FROM test_users";
     $stmt = $dbh -> query($sql);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     if (isset($result)){
-    echo "データ取得成功\n";
+    var_dump($result);
     }
     
+
 } catch (PDOException $e) {
     echo "接続失敗: " . $e->getMessage() . "\n";
     exit();
